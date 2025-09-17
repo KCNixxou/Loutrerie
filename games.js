402f65baaa4edeaa5f1133f28e9750920ceafe43
KCNixxou <steel.nixx@gmail.com>
2025-09-17 17:16:07 +0200

    Update games.js

--- games.js
+++ games.js
@@ -1809,27 +1809,6 @@ async function handleHighLow(interaction) {
   await interaction.reply({ embeds: [embed], components: [row] });
 }
 
-// Fonction pour créer un jeu de cartes
-function createDeck() {
-  const suits = ['♥', '♦', '♣', '♠'];
-  const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
-  const deck = [];
-  
-  for (const suit of suits) {
-    for (const value of values) {
-      deck.push({ suit, value });
-    }
-  }
-  
-  // Mélanger le jeu
-  for (let i = deck.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [deck[i], deck[j]] = [deck[j], deck[i]];
-  }
-  
-  return deck;
-}
-
 // Fonction pour vérifier si l'utilisateur a accès au High Low spécial
 function hasSpecialAccess(userId, channelId) {
   const { specialHighLow } = require('./config');
