bcdbddcb5d85557b46fb24e364bbe5ba1abe54dd
KCNixxou <steel.nixx@gmail.com>
2025-09-25 18:17:35 +0200

    Update index.js

--- games/index.js
+++ games/index.js
@@ -1,68 +1,16 @@
-// Importation des jeux
-const { handleMinesCommand, handleMinesButtonInteraction } = require('./mines');
-const { handleMinesMultiCommand, handleMinesMultiInteraction } = require('./mines-multi');
-const { handleSpecialMinesCommand, handleSpecialMinesInteraction } = require('./special-mines');
-const { 
-  handleHighLow, 
-  handleSpecialHighLow, 
-  handleHighLowAction, 
-  handleHighLowDecision 
-} = require('./highlow');
-const {
-  handleRouletteStart,
-  handleRouletteChoice
-} = require('./roulette');
+// games/index.js
 
-const {
-  handleSlots
-} = require('./slots');
-
-const {
-  handleBlackjackStart,
-  handleBlackjackAction,
-  resolveBlackjack
-} = require('./blackjack');
-
-// Exportation de tous les jeux
-module.exports = {
-  // Mines
-  handleMinesCommand,
-  handleMinesButtonInteraction,
-  
-  // Mines Multijoueur
-  handleMinesMultiCommand,
-  handleMinesMultiInteraction,
-  
-  // Mines Spéciales
-  handleSpecialMinesCommand,
-  handleSpecialMinesInteraction,
-  
-  // High Low
-  handleHighLow,
-  handleSpecialHighLow,
-  handleHighLowAction,
-  handleHighLowDecision,
-  
-  // Roulette
-  handleRouletteStart,
-  handleRouletteChoice,
-  
-  // Machine à sous
-  handleSlots,
-  
-  // Blackjack
-  handleBlackjackStart,
-  handleBlackjackAction,
-  resolveBlackjack,
-  handleCoinflipSolo: () => {},
-  handleCoinflipMulti: () => {},
-  handleConnectFour: () => {},
-  handleShop: () => {},
-  handlePurchase: () => {},
-  handleTicTacToe: () => {},
-  handleTicTacToeMove: () => {},
-  handleConnectFourMove: () => {},
-  getTicTacToeLeaderboard: () => {},
-  handleTicTacToeLeaderboard: () => {},
-  resetTicTacToeStats: () => {}
-};
+// Exporter toutes les fonctions de jeu en fusionnant les modules
+module.exports = Object.assign(
+  {},
+  require('./mines'),
+  require('./mines-multi'),
+  require('./special-mines'),
+  require('./highlow'),
+  require('./roulette'),
+  require('./slots'),
+  require('./blackjack'),
+  require('./coinflip'),
+  require('./shop'),
+  require('./tic-tac-toe')
+);
